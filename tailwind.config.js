a2a5dc99a62a3d10986e8a66ad737ede0c247713
Ebenezer Raphael <ebenezerraphael0@gmail.com>
2024-05-26 00:14:33 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    './*.html',
+  ],
+  theme: {
+    extend: {
+        fontFamily: {
+        'bsc': ['BarlowSemiCondensed', 'sans-serif'],
+      },
+        colors: {
+        'moderateviolet': '#7541c8',
+        'verydarkgrayishblue': '#48556a',
+        'verydarkblackishblue': '#19212e',
+        'lightgray': '#cfcfcf',
+        'lightgrayishblue': '#ecf2f8',
+            'white': '#ffffff',
+      }
+    },
+  },
+  plugins: [],
+}
